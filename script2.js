be0b3b142e8c6495e9ed635fa3491f51838c4ddd
pkm0707 <145551426+pkm0707@users.noreply.github.com>
2023-09-26 17:43:58 +0530

    Create script2.js for comparing two JSON
    Which have the same Properties without order

---
+++ script2.js
@@ -0,0 +1,10 @@
+let obj1 = {name:"Person 1",age:5};
+let obj2 = {age:5,name:"Person 1"};
+if (JSON.stringify(obj1) === JSON.stringify(obj2))
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
